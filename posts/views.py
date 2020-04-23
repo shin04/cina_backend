551202bfc8003a56fc3ac491e0bdefe3dee9d462
shin04 <daikon0413@gmail.com>
2020-04-23 23:25:01 +0900

    統合

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,6 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-
 class UserViewset(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
