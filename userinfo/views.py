5e5f1ecb6d60881633dc2f49030bcd2936c0606d
shin04 <daikon0413@gmail.com>
2020-04-24 16:07:19 +0900

    ワークススペース存在確認

--- userinfo/views.py
+++ userinfo/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import UserSerializer, WorkspaceSerializer, WorksapeTableSerializer
 from .models import Workspace, WorksapeTable
@@ -11,6 +13,14 @@ class UserViewset(viewsets.ModelViewSet):
 class WorkspaceViewset(viewsets.ModelViewSet):
     queryset = Workspace.objects.all()
     serializer_class = WorkspaceSerializer
+
+    @action(detail=False)
+    def exist_workspace(self, request):
+        workspace = Workspace.objects.all().filter(uuid__iexact=request.GET.get(id))
+        if workspace == None:
+            return Response({'exist': False})
+        else:
+            return Response({'exist': True})
 
 class WorkspaceTableViewset(viewsets.ModelViewSet):
     queryset = WorksapeTable.objects.all()
